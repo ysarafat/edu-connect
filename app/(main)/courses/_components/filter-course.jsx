0de847a4322edee1047f810a2d41da89c9509c29
Yeasir Arafat <ysarafat.bd@gmail.com>
2025-08-23 13:02:14 +0600

    Implement course filtering, sorting, and searching features; add ActiveFilters, FilterCourse, FilterCourseMobile, SearchCourse, and SortCourse components

---
+++ app/(main)/courses/_components/filter-course.jsx
@@ -0,0 +1,131 @@
+"use client";
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Checkbox } from "@/components/ui/checkbox";
+
+const CATEGORY_OPTIONS = [
+  {
+    id: 1,
+    label: "Design",
+    value: "design",
+  },
+
+  {
+    id: 3,
+    label: "Development",
+    value: "development",
+  },
+  {
+    id: 4,
+    label: "Marketing",
+    value: "marketing",
+  },
+  {
+    id: 5,
+    label: "IT & Software",
+    value: "it-software",
+  },
+  {
+    id: 6,
+    label: "Personal Development",
+    value: "personal-development",
+  },
+  {
+    id: 7,
+    label: "Business",
+    value: "business",
+  },
+  {
+    id: 8,
+    label: "Photography",
+    value: "photography",
+  },
+  {
+    id: 9,
+    label: "Music",
+    value: "music",
+  },
+];
+const PRICE_OPTIONS = [
+  { label: "Free", value: "free" },
+  { label: "Paid", value: "paid" },
+];
+const FilterCourse = () => {
+  return (
+    <div className="hidden lg:block">
+      <Accordion defaultValue={["categories"]} type="multiple">
+        {/* Categories filter */}
+        <AccordionItem value="categories">
+          <AccordionTrigger className="py-3 text-sm text-gray-400 hover:text-gray-500">
+            <span className="font-medium text-gray-900">Categories</span>
+          </AccordionTrigger>
+
+          <AccordionContent className="pt-6 animate-none">
+            <ul className="space-y-4">
+              {CATEGORY_OPTIONS.map((option, optionIdx) => (
+                <li key={option.value} className="flex items-center">
+                  <Checkbox
+                    type="checkbox"
+                    id={`category-${optionIdx}`}
+                    onCheckedChange={() => {
+                      applyArrayFilter({
+                        type: "categories",
+                        value: option.value,
+                      });
+                    }}
+                    // checked={filter?.categories?.includes(option.value)}
+                  />
+                  <label
+                    htmlFor={`category-${optionIdx}`}
+                    className="ml-3 text-sm text-gray-600 cursor-pointer"
+                  >
+                    {option.label}
+                  </label>
+                </li>
+              ))}
+            </ul>
+          </AccordionContent>
+        </AccordionItem>
+        {/* Price filter */}
+        <AccordionItem value="price">
+          <AccordionTrigger className="py-3 text-sm text-gray-400 hover:text-gray-500">
+            <span className="font-medium text-gray-900">Price</span>
+          </AccordionTrigger>
+
+          <AccordionContent className="pt-6 animate-none">
+            <ul className="space-y-4">
+              {PRICE_OPTIONS.map((option, optionIdx) => (
+                <li key={option.value} className="flex items-center">
+                  <Checkbox
+                    type="checkbox"
+                    id={`price-${optionIdx}`}
+                    onCheckedChange={() => {
+                      applyArrayFilter({
+                        type: "price",
+                        value: option.value,
+                      });
+                    }}
+                    // checked={filter.price.includes(option.value)}
+                  />
+                  <label
+                    htmlFor={`price-${optionIdx}`}
+                    className="ml-3 text-sm text-gray-600 cursor-pointer"
+                  >
+                    {option.label}
+                  </label>
+                </li>
+              ))}
+            </ul>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </div>
+  );
+};
+
+export default FilterCourse;
--- app/(main)/page.jsx
+++ app/(main)/page.jsx
@@ -1,12 +1,12 @@
 import { SectionTitle } from "@/components/section-title";
-import { Button, buttonVariants } from "@/components/ui/button";
-import { formatPrice } from "@/lib/formatPrice";
+import { buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { getCategories } from "@/queries/categories";
 import { getCourseList } from "@/queries/courses";
-import { ArrowRight, ArrowRightIcon, BookOpen } from "lucide-react";
+import { ArrowRightIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import CourseCard from "./courses/_components/course-card";
 
 const HomePage = async () => {
   const categories = await getCategories();
@@ -38,7 +38,10 @@ const HomePage = async () => {
             way.”
           </p>
           <div className="flex items-center gap-3 flex-wrap justify-center">
-            <Link href="" className={cn(buttonVariants({ size: "lg" }))}>
+            <Link
+              href="/courses"
+              className={cn(buttonVariants({ size: "lg" }))}
+            >
               Explore Now
             </Link>
             <Link
@@ -93,7 +96,7 @@ const HomePage = async () => {
         <div className="flex items-center justify-between">
           <SectionTitle>Courses</SectionTitle>
           <Link
-            href={""}
+            href={"/courses"}
             className=" text-sm font-medium  hover:opacity-80 flex items-center gap-1"
           >
             Browse All <ArrowRightIcon className="h-4 w-4" />
@@ -101,56 +104,7 @@ const HomePage = async () => {
         </div>
         <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-4">
           {courses.map((course) => {
-            return (
-              <Link key={course?.id} href={`/courses/${course?.id}`}>
-                <div className="group hover:shadow-sm transition overflow-hidden border rounded-lg p-3 h-full">
-                  <div className="relative w-full aspect-video rounded-md overflow-hidden">
-                    <Image
-                      src={`/assets/images/courses/${course?.thumbnail}`}
-                      alt={course?.title}
-                      className="object-cover"
-                      fill
-                    />
-                  </div>
-                  <div className="flex flex-col pt-2">
-                    <div className="text-lg md:text-base font-medium group-hover:text-sky-700 line-clamp-2">
-                      {course?.title}
-                    </div>
-                    <p className="text-xs text-muted-foreground">
-                      {course?.category?.title}
-                    </p>
-                    <div className="my-3 flex items-center gap-x-2 text-sm md:text-xs">
-                      <div className="flex items-center gap-x-1 text-slate-500">
-                        <div>
-                          <BookOpen className="w-4" />
-                        </div>
-                        <span>{course?.modules?.length} Chapters</span>
-                      </div>
-                    </div>
-
-                    {/* <CourseProgress
-                      size="sm"
-                      value={80}
-                      variant={110 === 100 ? "success" : ""}
-                    /> */}
-
-                    <div className="flex items-center justify-between mt-4">
-                      <p className="text-md md:text-sm font-medium text-slate-700">
-                        {formatPrice(course?.price)}
-                      </p>
-
-                      <Button
-                        variant="ghost"
-                        className="text-xs text-sky-700 h-7 gap-1"
-                      >
-                        Enroll
-                        <ArrowRight className="w-3" />
-                      </Button>
-                    </div>
-                  </div>
-                </div>
-              </Link>
-            );
+            return <CourseCard key={course?.id} course={course} />;
           })}
         </div>
       </section>
